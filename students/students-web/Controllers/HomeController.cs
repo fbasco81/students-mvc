55170ce9ab0b89d44f87dea91714fb9f56813e73
Francesco Basco <francesco.basco@msc.com>
2021-09-13 12:26:50 +0200

    update-delete-detail

--- students/students-web/Controllers/HomeController.cs
+++ students/students-web/Controllers/HomeController.cs
@@ -12,12 +12,38 @@ namespace students_web.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Students()
         {
             var studentService = new StudentService();
             var students = studentService.GetStudents();
             return View(students);
         }
- 
+
+        [HttpGet]
+        public ActionResult StudentDetail(int studentId)
+        {
+            var studentService = new StudentService();
+            var students = studentService.GetStudent(studentId);
+            return View(students);
+        }
+
+        [HttpPost]
+        public ActionResult SaveStudent(Student student)
+        {
+            var studentService = new StudentService();
+            var result = studentService.SaveStudent(student);
+
+            return View("StudentSaveResult", result);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteStudent(int studentId)
+        {
+            var studentService = new StudentService();
+            var result = studentService.DeleteStudent(studentId);
+
+            return View("StudentDeleteResult", result);
+        }
     }
 }
--- students/students-web/Services/StudentService.cs
+++ students/students-web/Services/StudentService.cs
@@ -29,7 +29,7 @@ namespace students_web.Services
                     {
                         Id = reader.GetInt32(reader.GetOrdinal("StudentId")),
                         Name = reader.GetString(reader.GetOrdinal("Firstname")),
-                        Surname = reader.GetString(reader.GetOrdinal("Firstname")),
+                        Surname = reader.GetString(reader.GetOrdinal("LastName")),
                     });
 
                 }
@@ -37,6 +37,98 @@ namespace students_web.Services
             
             return students;
         }
- 
+
+        public Student GetStudent(int studentId)
+        {
+            Student student = null;
+            var cnstr = ConfigurationManager.ConnectionStrings["StudentConnection"].ConnectionString;
+            using (var conn = new SqlConnection(cnstr))
+            {
+                conn.Open();
+                var command = conn.CreateCommand();
+                command.CommandText = "SELECT StudentId, FirstName, LastName " +
+                                        "FROM Student " +
+                                        "WHERE StudentId=@StudentId";
+
+                command.Parameters.AddWithValue("StudentId", studentId);
+
+                var reader = command.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    student = new Student()
+                    {
+                        Id = reader.GetInt32(reader.GetOrdinal("StudentId")),
+                        Name = reader.GetString(reader.GetOrdinal("Firstname")),
+                        Surname = reader.GetString(reader.GetOrdinal("LastName")),
+                    };
+                }
+            }
+
+            return student;
+        }
+
+        public int SaveStudent(Student newStudent)
+        {
+            var student = GetStudent(newStudent.Id);
+            if (student == null)
+            {
+                student = new Student() { Id = -1 };
+            }
+            else
+            {
+                student.Id = newStudent.Id;
+            }
+            
+            student.Name = newStudent.Name;
+            student.Surname = newStudent.Surname;
+
+            int result;
+
+            var cnstr = ConfigurationManager.ConnectionStrings["StudentConnection"].ConnectionString;
+            using (var conn = new SqlConnection(cnstr))
+            {
+                conn.Open();
+                var command = conn.CreateCommand();
+
+                if (student.Id <= 0) // it's a new student
+                {
+                    command.CommandText = "INSERT INTO Student (FirstName, LastName) VALUES (@FirstName, @LastName) ";
+                }
+                else
+                {
+                    command.CommandText = "UPDATE Student SET FirstName=@FirstName, LastName=@LastName WHERE StudentId=@StudentId ";
+                    command.Parameters.AddWithValue("StudentId", student.Id);
+                }
+
+                command.Parameters.AddWithValue("FirstName", student.Name);
+                command.Parameters.AddWithValue("LastName", student.Surname);
+
+                result = command.ExecuteNonQuery();
+            }
+
+            return result;
+        }
+
+        public int DeleteStudent(int studentId)
+        {
+            int result;
+
+            var cnstr = ConfigurationManager.ConnectionStrings["StudentConnection"].ConnectionString;
+            using (var conn = new SqlConnection(cnstr))
+            {
+                conn.Open();
+                var command = conn.CreateCommand();
+
+                command.CommandText = "DELETE FROM Student WHERE StudentId=@StudentId ";
+                command.Parameters.AddWithValue("StudentId", studentId);
+                
+                result = command.ExecuteNonQuery();
+            }
+
+            return result;
+        }
+
     }
 }
